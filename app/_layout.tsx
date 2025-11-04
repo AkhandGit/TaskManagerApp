b655bb8cb8b07fa3c51ff5afaf38134cd4ddbe7d
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-11-04 13:14:34 +0530

    made changes to layout file to remove error of adding task

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,15 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { TaskProvider } from "./context/TaskContext"; 
 
 export default function RootLayout() {
   return (
-    <>
+    <TaskProvider>   
       <Stack>
         <Stack.Screen name="index" options={{ title: "Tasks" }} />
         <Stack.Screen name="add-task" options={{ title: "Add Task" }} />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </TaskProvider>
   );
 }
